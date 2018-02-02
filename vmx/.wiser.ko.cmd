c62a32147c1afc52f61df27d90384645cf26543b
Aseem Sethi <aseemsethi@yahoo.com>
2018-02-02 09:26:13 -0500

    home pc regs

---
+++ vmx/.wiser.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/asethi/kvm/vmx/wiser.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/asethi/kvm/vmx/wiser.ko /home/asethi/kvm/vmx/wiser.o /home/asethi/kvm/vmx/wiser.mod.o
